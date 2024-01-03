ab3d5355c83f5359e1001526f2d3b77fb03560f9
angelina7920 <angelina.skutskaya11@gmail.com>
2024-01-03 15:54:21 +0300

    feat: add routes to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,24 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { DefSelect } from '../../interfaces/default-select.interface';
 import { DefaultSelectComponent } from '../default-select/default-select.component';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../../../app-routing.module';
-import { ActivatedRoute, Router } from '@angular/router';
+import {  NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [DefaultSelectComponent, CommonModule, AppRoutingModule],
+  imports: [DefaultSelectComponent, CommonModule, AppRoutingModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   constructor(
     private translate: TranslateService,
     private router: Router,
-    private route: ActivatedRoute
+    private cdr: ChangeDetectorRef
   ) {
     this.translate.setDefaultLang('en');
   }
@@ -30,4 +36,21 @@ export class HeaderComponent {
     this.translate.use(language);
   }
 
+  navLink: string = '';
+  public navtext: string = '';
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        if (event.url === '/') {
+          this.navLink = '/authentication/registration';
+          this.navtext = 'BUTTONS.AUTH';
+          this.cdr.detectChanges();
+        } else if (event.url === '/authentication/registration') {
+          this.navLink = '/';
+          this.navtext = 'BUTTONS.MAIN';
+          this.cdr.detectChanges();
+        }
+      });
+  }
 }
